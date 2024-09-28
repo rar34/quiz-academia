ad8b91847b09c664d6925a37756810ca972b95c4
rar34 <ra.cse34@gmail.com>
2024-09-28 20:01:21 +0600

    change logo size for mobile design

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content text-primary text-white bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            className="menu menu-sm dropdown-content text-primary bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                             {
                                 navItems.map(item => (
                                     <Link className={`${pathName === item.path && 'text-secondary border-b-2 border-secondary'} `} key={item.path} href={item.path}>{item.title}</Link>
@@ -56,7 +56,7 @@ const Navbar = () => {
                             }
                         </ul>
                     </div>
-                    <Link href={"/"} className="font-bold border-2 border-gray-200 rounded-md p-2 text-gray-200 text-3xl">Quiz<span className='text-secondary'>Academia</span></Link>
+                    <Link href={"/"} className="font-bold border-2 border-gray-200 rounded-md p-2 text-gray-200 text-xl md:text-3xl">Quiz<span className='text-secondary'>Academia</span></Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <div className='flex gap-6 text-xl text-white'>
