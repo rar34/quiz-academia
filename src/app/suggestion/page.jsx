2637b297af514fa7eef8d53be04c12c1e8b5b050
rar34 <ra.cse34@gmail.com>
2024-09-30 20:34:04 +0600

    created suggestion component

---
+++ src/app/suggestion/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='min-h-[70vh] container mx-auto my-10'>
+            Suggestion will be available soon............
+        </div>
+    );
+};
+
+export default page;
