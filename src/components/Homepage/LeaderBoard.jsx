eb4ee563f03519d2468d97bc7426e8b7ca5c58fc
rar34 <ra.cse34@gmail.com>
2024-10-08 20:34:23 +0600

    update leaderboard

--- src/components/Homepage/LeaderBoard.jsx
+++ src/components/Homepage/LeaderBoard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LeaderBoard = () => {
     return (
         <div>
-            Leader board will be available soon............
+            <h1>Leader board will be available soon............</h1>
         </div>
     );
 };
