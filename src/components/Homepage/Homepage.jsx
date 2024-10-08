448a83a765bc826b77355bde2166721a06c75e1e
rar34 <ra.cse34@gmail.com>
2024-10-08 20:55:05 +0600

    fixed bugs

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
-import LeaderBoard from './Leaderboard';
+import LeaderBoard from './LeaderBoard';
 
 const Homepage = () => {
     return (
