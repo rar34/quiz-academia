24cf53f9e6c0eaa2a7e1d4ad482ee21da8ae8770
rar34 <ra.cse34@gmail.com>
2024-10-08 20:43:50 +0600

    added card in leaderboard

--- src/components/Homepage/LeaderBoard.jsx
+++ src/components/Homepage/LeaderBoard.jsx
@@ -1,9 +1,23 @@
+import Image from 'next/image';
 import React from 'react';
 
 const LeaderBoard = () => {
     return (
-        <div>
-            <h1>Leader board will be available soon............</h1>
+        <div className="container mx-auto">
+            <div className="card card-side bg-base-100 shadow-xl">
+                <figure>
+                    <Image height={100} width={100}
+                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                        alt="Movie" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">New movie is released!</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
