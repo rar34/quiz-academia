00812880aafbd5a837bfd16e4910ab9f4edbc542
rar34 <ra.cse34@gmail.com>
2024-10-08 19:58:19 +0600

    added banner

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Homepage = () => {
     return (
         <div className='bg-slate-200'>
             <div className='container mx-auto py-10 min-h-[65vh]'>
-                <h3 className='text-5xl font-bold text-primary'>পরীক্ষা দেওয়ার জন্য Quiz Tab এ ক্লিক কর । </h3>
+                <Banner />
             </div>
         </div>
     );
