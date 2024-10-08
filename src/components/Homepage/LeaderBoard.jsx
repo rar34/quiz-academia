8f1d03774447c2e3dcbcd33e60e06b042c0e84ee
rar34 <ra.cse34@gmail.com>
2024-10-08 21:18:59 +0600

    added some profile in leader board

--- src/components/Homepage/LeaderBoard.jsx
+++ src/components/Homepage/LeaderBoard.jsx
@@ -3,18 +3,84 @@ import React from 'react';
 
 const Leaderboard = () => {
     return (
-        <div className="container mx-auto">
-            <div className="card card-side bg-base-100 shadow-xl">
-                <figure>
-                    <Image height={100} width={100}
-                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                        alt="Movie" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">New movie is released!</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Watch</button>
+        <div className="container mx-auto my-14">
+            <h2 className="text-5xl my-14 text-center border-b-2 pb-4 font-semibold text-primary">Leader Board</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="card card-side bg-base-100 border-2 border-secondary p-4">
+                    <figure>
+                        <Image height={100} width={100}
+                            src="https://i.ibb.co.com/FDpGqc0/pp.jpg"
+                            alt="Movie" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">John English</h2>
+                        <p>Doing great</p>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 border-2 border-secondary p-4">
+                    <figure>
+                        <Image height={100} width={100}
+                            src="https://i.ibb.co.com/FDpGqc0/pp.jpg"
+                            alt="Movie" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">John English</h2>
+                        <p>Doing great</p>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 border-2 border-secondary p-4">
+                    <figure>
+                        <Image height={100} width={100}
+                            src="https://i.ibb.co.com/FDpGqc0/pp.jpg"
+                            alt="Movie" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">John English</h2>
+                        <p>Doing great</p>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 border-2 border-secondary p-4">
+                    <figure>
+                        <Image height={100} width={100}
+                            src="https://i.ibb.co.com/FDpGqc0/pp.jpg"
+                            alt="Movie" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">John English</h2>
+                        <p>Doing great</p>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 border-2 border-secondary p-4">
+                    <figure>
+                        <Image height={100} width={100}
+                            src="https://i.ibb.co.com/FDpGqc0/pp.jpg"
+                            alt="Movie" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">John English</h2>
+                        <p>Doing great</p>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 border-2 border-secondary p-4">
+                    <figure>
+                        <Image height={100} width={100}
+                            src="https://i.ibb.co.com/FDpGqc0/pp.jpg"
+                            alt="Movie" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">John English</h2>
+                        <p>Doing great</p>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 border-2 border-secondary p-4">
+                    <figure>
+                        <Image height={100} width={100}
+                            src="https://i.ibb.co.com/FDpGqc0/pp.jpg"
+                            alt="Movie" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">John English</h2>
+                        <p>Doing great</p>
                     </div>
                 </div>
             </div>
