15694cf7520c9fdfb4a851605b933b496142f192
rar34 <ra.cse34@gmail.com>
2024-10-12 23:26:55 +0600

    implemented signup method and save user into mongodb

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { FaSquareFacebook } from 'react-icons/fa6';
 
 const SignUp = () => {
-    const handleSignUp = (e) => {
+    const handleSignUp = async (e) => {
         e.preventDefault();
         const form = e.target;
 
@@ -14,7 +14,18 @@ const SignUp = () => {
             email: form.email.value,
             password: form.password.value
         }
-        console.log(newUser)
+        // console.log(newUser)
+        const resp = await fetch("http://localhost:3000/signup/api", {
+            method: "POST",
+            body: JSON.stringify(newUser),
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+        console.log(resp)
+        if (resp.status === 200) {
+            e.target.reset()
+        }
     }
 
 
@@ -65,7 +76,7 @@ const SignUp = () => {
                 </div>
 
                 <div className="flex items-center mt-6 -mx-2">
-                    <button type="button" className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none">
+                    <button type="button" className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-primary rounded-lg">
                         <svg className="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
                             <path d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z">
                             </path>
