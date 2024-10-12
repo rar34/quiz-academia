ffdf49afd2aa54a47f45f9c035ff2aa7a1c0d9b1
rar34 <ra.cse34@gmail.com>
2024-10-12 23:04:26 +0600

    make password secured with hashpassword

--- src/app/signup/api/route.js
+++ src/app/signup/api/route.js
@@ -1,3 +1,22 @@
-export const POST = async(request) =>{
-    
+import { connectDB } from "@/lib/connectDB";
+import bcrypt from "bcrypt";
+
+
+export const POST = async (request) => {
+    const newUser = await request.json();
+
+    try {
+        const db = await connectDB();
+        const userCollection = await db.collection("users");
+        const exits = await userCollection.findOne({ email: newUser.email });
+        if (exits) {
+            return Response.json({ message: "User already exists" }, { status: 304 });
+        }
+        const hashedPassword = bcrypt.hashSync(newUser.password, 14);
+        const resp = await userCollection.insertOne({ ...newUser, password: hashedPassword });
+        return Response.json({ message: "User created successfully" }, { status: 200 })
+
+    } catch (error) {
+        return Response.json({ message: "Something went wrong" }, { status: 400 })
+    }
 }
