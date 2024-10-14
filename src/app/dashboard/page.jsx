004043a96a73e9fde5ffa8ff005391d251e90d6e
rar34 <ra.cse34@gmail.com>
2024-10-14 23:06:16 +0600

    created dashboard component

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            
+            <Link href={"/"}>Home</Link>
+        </div>
+    );
+};
+
+export default Dashboard;
--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
 import { signOut } from "next-auth/react"
+import Dashboard from '@/app/dashboard/page';
 
 const Navbar = () => {
     const pathName = usePathname();
@@ -33,6 +34,15 @@ const Navbar = () => {
 
     const session = useSession()
     // console.log(session)
+
+    // if (pathName.includes('dashboard')) {
+    //     return (
+    //         <div className='bg-primary min-h-[65vh] p-4 flex gap-6 justify-center text-center text-white'>
+    //             {/* <Link href={"/"}>Home</Link> */}
+    //             <Dashboard />
+    //         </div>
+    //     )
+    // }
 
     return (
         <div className='sticky top-0 bg-primary py-4 shadow-md'>
@@ -65,7 +75,7 @@ const Navbar = () => {
                     </div>
                     {/* <Link href={"/"} className="font-bold border-2 border-gray-200 rounded-md p-2 text-gray-200 text-xl md:text-3xl">Quiz<span className='text-secondary'>Academia</span></Link> */}
                     <Link href="/">
-                            <Image src="/assets/quiz-academia.png" height={100} width={300} alt="quiz-academia" />
+                        <Image src="/assets/quiz-academia.png" height={100} width={300} alt="quiz-academia" />
                     </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
@@ -82,8 +92,28 @@ const Navbar = () => {
                         session.data && <p className='text-white mr-4 text-lg'>Hi, <span className='italic'>{session?.data?.user?.name}</span></p>
                     }
                     {
-                        !session.data ? <Link href={"/login"} className="btn btn-outline btn-secondary">Login</Link>:
-                        <button className="btn btn-outline btn-secondary" onClick={()=> signOut()}>Logout</button>
+                        !session.data ? <Link href={"/login"} className="btn btn-outline btn-secondary">Login</Link> :
+                            // <button className="btn btn-outline btn-secondary" onClick={()=> signOut()}>Logout</button>
+                            <div className="dropdown dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <Image height={20} width={20}
+                                            alt="Tailwind CSS Navbar component"
+                                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                    </div>
+                                </div>
+                                <ul
+                                    tabIndex={0}
+                                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                                    <li>
+                                        <Link href={"/profile"} className="justify-between hover:text-secondary transition">
+                                            Profile
+                                        </Link>
+                                    </li>
+                                    <li><Link className="hover:text-secondary transition" href={"/dashboard"}>Dashboard</Link></li>
+                                    <li><button className="hover:text-secondary transition" onClick={()=> signOut()}>Logout</button></li>
+                                </ul>
+                            </div>
                     }
                 </div>
             </div>
